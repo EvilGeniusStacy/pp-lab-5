935854053e42b70345c02796523877dfdbf6d12c
Robert Matuszewski <robert.matuszewski556@gmail.com>
2024-05-04 10:16:52 +0200

    h

--- Main.java
+++
@@ -1,8 +0,0 @@
-import company.implementations.EmailMessenger;
-
-public class Main {
-    public static void main(String[] args) {
-        EmailMessenger emsg = new EmailMessenger();
-        emsg.sendMessage("wiadomosc");
-    }
-}
